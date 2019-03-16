fdc78e9299daac7b882143ada42fc5b0a55b9198
CreativeGP <social@cretgp.com>
2019-03-17 00:47:50 +0900

    fix

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
+ 
+
+
