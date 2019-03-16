21530fde79ce3b6f07ab001ebdf615134ab6c445
CreativeGP <social@cretgp.com>
2019-03-17 00:57:12 +0900

    changed

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+'use strict';
 
-app.get('/', (req, res) => {
-    
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
 });
 
-io.on('connection', socket => {
-    socket.on('fb watch', (msg) => {
-        io.emit('fb watch', 'hello');
-    })
-})
-
-server.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
- 
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
 
 
